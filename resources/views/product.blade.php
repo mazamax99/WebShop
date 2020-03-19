49e46f1ce0aa5e99e60ac4401b2cdc18f51c9bfe
MaximPotsukov <ziperziperziperziper@gmail.com>
2020-03-19 15:46:38 +0600

    Создание шаблонов страниц и их настройка,добавление страниц(Корзина,Оформление заказа)

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,53 +1,6 @@
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Интернет Магазин: Товар</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <script src="/js/jquery.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/starter-template.css" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="http://internet-shop.tmweb.ru">Интернет Магазин</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li ><a href="http://internet-shop.tmweb.ru">Все товары</a></li>
-                <li ><a href="http://internet-shop.tmweb.ru/categories">Категории</a>
-                </li>
-                <li ><a href="http://internet-shop.tmweb.ru/basket">В корзину</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/reset">Сбросить проект в начальное состояние</a></li>
-                <li><a href="http://internet-shop.tmweb.ru/locale/en">en</a></li>
-
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">₽<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="http://internet-shop.tmweb.ru/currency/RUB">₽</a></li>
-                        <li><a href="http://internet-shop.tmweb.ru/currency/USD">$</a></li>
-                        <li><a href="http://internet-shop.tmweb.ru/currency/EUR">€</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="http://internet-shop.tmweb.ru/login">Войти</a></li>
-
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<div class="container">
+@extends('master')
+@section('title', 'Товар')
+@section('content')
     <div class="starter-template">
         <h1>iPhone X 64GB</h1>
         <h2>{{$product}}</h2>
@@ -61,6 +14,4 @@
 
             <input type="hidden" name="_token" value="oKMN2NLNPrtEbUmYspbxKj0h2USRo3n6bRXiWvbQ">        </form>
     </div>
-</div>
-</body>
-</html>
+@endsection
