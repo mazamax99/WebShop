1ccdc204f6d5429a37ce30ab76f489a1d73664df
mazamax99 <ziperziperziperziper@gmail.com>
2020-03-26 22:16:59 +0600

    Реализация REST патерна для управления товарами

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,15 +1,14 @@
 @extends('master')
 @section('title', 'Товар')
 @section('content')
-        <h1>iPhone X 64GB</h1>
-        <h2>{{$product}}</h2>
-        <h2>Мобильные телефоны</h2>
-        <p>Цена: <b>71990 ₽</b></p>
-        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
-        <p>Отличный продвинутый телефон с памятью на 64 gb</p>
+        <h1>{{$product->name}}</h1>
+        <h2>{{$product->category->name}}</h2>
+        <p>Цена: <b>{{$product->price}}</b></p>
+        <img src={{\Illuminate\Support\Facades\Storage::url($product->image)}}>
+        <p>{{$product->description}}</p>
 
-        <form action="http://internet-shop.tmweb.ru/basket/add/1" method="POST">
+        <form action="{{route('basket-add',$product)}}" method="POST">
             <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
 
-            <input type="hidden" name="_token" value="oKMN2NLNPrtEbUmYspbxKj0h2USRo3n6bRXiWvbQ">        </form>
+            @csrf        </form>
 @endsection
